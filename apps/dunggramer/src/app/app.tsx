a67ff04ee8fe3d1681716f758d66c40b883389aa
DungGramer <dung.dev.gramer@gmail.com>
2023-02-13 22:34:35 +0700

    Add about

--- apps/dunggramer/src/app/app.tsx
+++ apps/dunggramer/src/app/app.tsx
@@ -1,5 +1,6 @@
 import Home from 'apps/dunggramer/src/app/components/home/Home';
 import Header from './components/header';
+import About from '@/src/app/components/about/About';
 
 export function App() {
   return (
@@ -7,6 +8,7 @@ export function App() {
       <Header />
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   );
