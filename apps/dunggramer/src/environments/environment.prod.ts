4d9360df2490c692b1b27ef2345bb6b6f30749e3
DungGramer <dung.dev.gramer@gmail.com>
2023-04-08 11:47:25 +0700

    delete react app

--- apps/dunggramer/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  EMAILJS_SERVICE_ID: 'service_3p9frkt',
-  EMAILJS_TEMPLATE_ID: 'template_0z18f2f',
-  EMAILJS_PUBLIC_KEY: 'Q85d7DKHi0gV6ZUCE',
-};
--- apps/dunggramer/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// When building for production, this file is replaced with `environment.prod.ts`.
-
-export const environment = {
-  production: false,
-  EMAILJS_SERVICE_ID: 'service_3p9frkt',
-  EMAILJS_TEMPLATE_ID: 'template_0z18f2f',
-  EMAILJS_PUBLIC_KEY: 'Q85d7DKHi0gV6ZUCE',
-};
